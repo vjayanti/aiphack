1a1516a1cb82b1090b79a329c455058bbe000dbc
Vasishta Jayanti <vasishta@box.com>
2018-12-06 01:54:19 -0800

    Next commit

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
 import json
 import os
+from subprocess import call
+import base64
 
 class ManagerServer(ThreadingMixIn, HTTPServer):
     def __init__(self, address, handler):
@@ -13,23 +15,18 @@ class ManagerRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         print(self.path)
         try:
-            if self.path == "/cover_shifts":
-                start = self.headers.getheader('start')
-                end = self.headers.getheader('end')
-                group_id = self.headers.getheader('group')
-                print(message)
+            if self.path == "/decrypt":
+                username = self.headers.getheader('username')
+                password = self.headers.getheader('password')
+                content_len = int(self.headers.getheader('content-length', 0))
+                file = self.rfile.read(content_len)
+                with open('file.in.docx', 'w') as tmp:
+                     tmp.write(file)
                 self.send_response(200)
                 self.end_headers()
-                self.wfile.write(message)
-            elif self.path == "/swap_shifts":
-                old_time = self.headers.getheader('old_time')
-                new_time = self.headers.getheader('new_time')
-                from_person = self.headers.getheader('from_person')
-                to_person = self.headers.getheader('to_person')
-                print(old_time, new_time, from_person, to_person)
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(message)
+                call(["./mip_driver.decrypt.exe", "--username", username, "--password", password, "--file", "file.in.docx", "--fileOut", "file.tmp.docx", "--decrypt"])
+                with open('file.out.tmp') as tmp:
+                     self.wfile.write(base64.b64decode(tmp.read()))
             else:
                 self.send_response(404)
         except Exception as e:
@@ -43,7 +40,40 @@ class ManagerRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write('hello world')
-
+        elif self.path == "/checkIsProtected":
+            username = self.headers.getheader('username')
+            password = self.headers.getheader('password')
+            content_len = int(self.headers.getheader('content-length', 0))
+            file = self.rfile.read(content_len)
+            with open('file.in.docx', 'w') as tmp:
+                 tmp.write(file)
+            exitCode = call(["./mip_driver.exe", "--username", username, "--password", password, "--file", "file.in.docx", "--checkIsProtected"])
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(exitCode)
+        elif self.path == "/checkIsLabeled":
+            username = self.headers.getheader('username')
+            password = self.headers.getheader('password')
+            content_len = int(self.headers.getheader('content-length', 0))
+            file = self.rfile.read(content_len)
+            with open('file.in.docx', 'w') as tmp:
+                 tmp.write(file)
+            exitCode = call(["./mip_driver.exe", "--username", username, "--password", password, "--file", "file.in.docx", "--checkIsLabeled"])
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(exitCode)
+        elif self.path == "/checkHasWatermark":
+            username = self.headers.getheader('username')
+            password = self.headers.getheader('password')
+            content_len = int(self.headers.getheader('content-length', 0))
+            file = self.rfile.read(content_len)
+            with open('file.in.docx', 'w') as tmp:
+                 tmp.write(file)
+            exitCode = call(["./mip_driver.exe", "--username", username, "--password", password, "--file", "file.in.docx", "--checkHasWatermark"])
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(exitCode)
+   
 PORT = 10000
 
 Handler = ManagerRequestHandler
